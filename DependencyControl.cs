baf2776984db97ccea926b68e2a9f416744d395e
skoggy <erikskoglund88@gmail.com>
2016-02-22 13:53:07 +0100

    init

---
+++ DependencyControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShadowCore
+{
+    public static class DependencyControl
+    {
+        public static string IncludeTypeAssemblyDependency(this Type type)
+        {
+            return type.ToString();
+        }
+    }
+}
